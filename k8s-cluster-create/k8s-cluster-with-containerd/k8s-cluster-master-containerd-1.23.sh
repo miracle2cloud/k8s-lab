1a0a274b17c18c7b78e84ca9a2a6227ba0845603
miracle2cloud <108317408+miracle2cloud@users.noreply.github.com>
2023-03-07 16:54:53 +0530

    Update k8s-cluster-master-containerd-1.23.sh

--- k8s-cluster-create/k8s-cluster-with-containerd/k8s-cluster-master-containerd-1.23.sh
+++ k8s-cluster-create/k8s-cluster-with-containerd/k8s-cluster-master-containerd-1.23.sh
@@ -34,6 +34,5 @@ sudo kubeadm init --upload-certs --kubernetes-version=v1.23.0  --control-plane-e
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 kubectl taint node master node-role.kubernetes.io/master:NoSchedule-
