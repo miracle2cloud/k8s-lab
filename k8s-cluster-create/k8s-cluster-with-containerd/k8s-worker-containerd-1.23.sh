3bdabee7d1b3a111f90e6e85dffeed5598e2a538
miracle2cloud <108317408+miracle2cloud@users.noreply.github.com>
2023-03-07 14:07:16 +0530

    Update k8s-worker-containerd-1.23.sh

--- k8s-cluster-create/k8s-cluster-with-containerd/k8s-worker-containerd-1.23.sh
+++ k8s-cluster-create/k8s-cluster-with-containerd/k8s-worker-containerd-1.23.sh
@@ -1,20 +1,15 @@
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "step1"
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 echo "kubeadm install"
-sudo apt update -y
+sudo apt update
 sudo apt -y install vim git curl wget kubelet=1.23.0-00 kubeadm=1.23.0-00 kubectl=1.23.0-00
+sudo apt-mark hold kubelet kubeadm kubectl
 echo "memory swapoff"
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
 sudo modprobe overlay
 sudo modprobe br_netfilter
-sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOF
-sysctl --system
 sudo tee /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
@@ -24,13 +19,13 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
-sysctl --system
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo sysctl --system
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y containerd.io
-mkdir -p /etc/containerd
-containerd config default>/etc/containerd/config.toml
+sudo mkdir -p /etc/containerd
+sudo bash -c "containerd config default > /etc/containerd/config.toml"
 sudo systemctl restart containerd
 sudo systemctl enable containerd  
 sudo systemctl enable kubelet 
